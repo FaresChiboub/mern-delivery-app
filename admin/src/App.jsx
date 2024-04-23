5b92f5c5f88042ae98ddb47727b6f59e0314a1e8
fares <fareschiboub10@gmail.com>
2024-04-23 13:50:03 +0200

    update

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,7 +8,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  const url = "http://localhost:5001";
+  const url = "https://mern-delivery-app-1.onrender.com";
   return (
     <div>
       <ToastContainer />
