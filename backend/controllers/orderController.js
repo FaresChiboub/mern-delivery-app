ab901014a7a908d6e50f397dee3c9ef430964aab
fares <fareschiboub10@gmail.com>
2024-04-23 15:01:34 +0200

    update

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -47,7 +47,7 @@ const placeOrder = async (req, res) => {
       success_url: `${frontend_url}/myOrders/verify?success=true&orderId=${newOrder._id}`,
       cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
     });
-    });
+  
     res.json({ success: true, session_url: session.url });
   } catch (error) {
     console.log(error);
