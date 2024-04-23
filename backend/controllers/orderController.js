5d471a289836ab8da2725208d8ce38766c87abf7
fares <fareschiboub10@gmail.com>
2024-04-23 14:02:38 +0200

    update

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // Placing user order for frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = "https://6627827a0460f7c601664213--frontenddeliveryapp.netlify.app/";
+  const frontend_url = "https://main--frontenddeliveryapp.netlify.app/";
 
   try {
     const newOrder = new orderModel({
@@ -44,8 +44,9 @@ const placeOrder = async (req, res) => {
     const session = await stripe.checkout.sessions.create({
       line_items: line_items,
       mode: "payment",
-      success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-      cancel_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+      success_url: `${frontend_url}/myOrders/verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+    });
     });
     res.json({ success: true, session_url: session.url });
   } catch (error) {
