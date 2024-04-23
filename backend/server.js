eeed4ca59ed2d1cf2af34eba1ab9b3e95dddc6a6
fares <fareschiboub10@gmail.com>
2024-04-23 11:15:41 +0200

    update

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,8 @@ const app = express();
 const PORT = process.env.PORT || 3111;
 
 // Middleware
-app.use(cors()); 
-app.use(express.json(
-  
-));
+app.use(cors());
+app.use(express.json());
 
 // Connect to the database
 connectToDatabase();
