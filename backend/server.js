8840c6d207232585d5bf6930118312f6aed7df1f
fares <fareschiboub10@gmail.com>
2024-04-23 11:00:56 +0200

    update

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 
 app.get("/", async (req, res) => {
-  res.send("API WORKING");
+  res.send("Server running");
 });
 
 // Start the server
