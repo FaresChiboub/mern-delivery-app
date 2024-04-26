982bc9fffe38be7d13f6764535cbda3abe484b01
fares <fareschiboub10@gmail.com>
2024-04-26 13:59:53 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ dotenv.config();
 const backendUrl = process.env.BACKEND_URL || 'http://localhost:5001';
 // App config
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 
 // Middleware
 app.use(cors());
